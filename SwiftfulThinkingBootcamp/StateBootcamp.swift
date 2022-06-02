51fa70e0a03daf0b3a2777fb40298058ab8fb11d
jezstdio <jezstdio@protonmail.com>
2022-06-02 13:18:20 +0200

    add state bootcamp

---
+++ SwiftfulThinkingBootcamp/StateBootcamp.swift
@@ -0,0 +1,50 @@
+//
+//  StateBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jez Gábor on 2022. 06. 02..
+//
+
+import SwiftUI
+
+struct StateBootcamp: View {
+    
+    @State var backgroundColor: Color = Color.green
+    @State var myTitle: String = "My Title"
+    @State var count: Int = 0
+    
+    var body: some View {
+        ZStack {
+            backgroundColor.ignoresSafeArea(.all)
+            
+            VStack(spacing: 20) {
+                Text(myTitle)
+                    .font(.title)
+                Text("Count: \(count)")
+                    .font(.headline)
+                    .underline()
+                
+                HStack(spacing: 20) {
+                    Button("Button 1") {
+                        backgroundColor = .red
+                        myTitle = "Button 1 was pressed"
+                        count += 1
+                    }
+                    
+                    Button("Button 2") {
+                        backgroundColor = .purple
+                        myTitle = "Button 2 was pressed"
+                        count -= 1
+                    }
+                }
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+struct StateBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StateBootcamp()
+    }
+}
