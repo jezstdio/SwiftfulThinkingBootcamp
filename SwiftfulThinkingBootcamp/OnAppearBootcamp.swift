5e0a2ab78eb97328d8a1fe27bf50a087a03f548f
jezstdio <jezstdio@protonmail.com>
2022-06-07 00:55:54 +0200

    add onAppear bootcamp

---
+++ SwiftfulThinkingBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,51 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jez Gábor on 2022. 06. 07..
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    
+    @State var myText: String = "Start text."
+    @State var count: Int = 0
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Text(myText)
+                
+                LazyVStack {
+                    ForEach(0..<50) { _ in
+                        RoundedRectangle(cornerRadius: 24.0)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear {
+                                count += 1
+                            }
+                            .onDisappear {
+                                count -= 1
+                            }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    myText = "This is the new text!"
+                }
+            })
+            .onDisappear(perform: {
+                myText = "Ending text."
+            })
+            .navigationTitle("onAppear Bootcamp \(count)")
+        }
+    }
+}
+
+struct OnAppearBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        OnAppearBootcamp()
+    }
+}
